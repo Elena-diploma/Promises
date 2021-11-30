2113654c9600f696ba964d998c2ca3a5ed29e488
Elena Tursunova <elenatursunova@gmail.com>
2021-11-30 18:57:36 +0300

    исправления

--- src/js/gameSavingLoader.js
+++ src/js/gameSavingLoader.js
@@ -3,6 +3,8 @@ import read from './reader';
 
 export default class GameSavingLoader {
   static load() {
-    return read().then(json);
+    return read()
+        .then((response) => json(response))
+        .then((response) => JSON.parse(response));
   }
 }
